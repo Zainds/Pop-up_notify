4396298d356a4e7093e59ae35e3b252752110fcb
Ангальдт Михаил <beat1097@mail.ru>
2023-03-15 20:58:45 +0700

    пррп

---
+++ app/src/main/java/com/example/pop_up_notify/afterNotification.kt
@@ -0,0 +1,11 @@
+package com.example.pop_up_notify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class afterNotification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_notification)
+    }
+}
